e2fb38f9f4268db3ae3857dc4344d4d908d3fe80
Helena Coppieters <helenacoppieters@yahoo.com>
2020-04-12 19:36:10 +0200

    Deel 1 van de videoreeks

---
+++ EmployeeManager.Mvc/Controllers/EmployeeManagerController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManager.Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManager.Mvc.Controllers
+{
+    public class EmployeeManagerController : Controller
+    {
+        private AppDbContext db = null;
+        public EmployeeManagerController(AppDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult List()
+        {
+            List<Employee> model = (from e in db.Employees
+                                    orderby e.EmployeeID
+                                    select e).ToList();
+            return View(model);
+        }
+    }
+}
